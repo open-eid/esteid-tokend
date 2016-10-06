0f89919808864b284f3ef35b182b80726ce936f9
Martin Paljak <martin@martinpaljak.net>
2016-10-06 13:23:50 +0300

    Only sudo things that require sudo

--- clean_install.sh
+++ clean_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Clean token cache!"
-sudo rm -rf /var/db/TokenCache/tokens/
-sudo mkdir /var/db/TokenCache/tokens/
+sudo rm -rf /var/db/TokenCache/tokens/*
 echo "Remove the old version!"
 sudo rm -rf /Library/Security/tokend/EstEID.tokend/
 echo "Build and install!"
-sudo xcodebuild -project EstEID.tokend/Tokend.xcodeproj -configuration Development clean install DSTROOT=/
+xcodebuild -project EstEID.tokend/Tokend.xcodeproj -configuration Development clean build
+sudo mv EstEID.tokend/build/EstEID.tokend /Library/Security/tokend/EstEID.tokend
